a1148636418c5b8700f5f649981bd1ab9b1cc4e4
Fadhillaypa <fadhillaypa@gmail.com>
2023-10-12 19:56:58 +0700

    prak 1

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,19 +30,12 @@ class Database extends Config
         'username'     => '',
         'password'     => '',
         'database'     => '',
+        'username'     => 'root',
+        'password'     => 'mysql',
+        'database'     => 'backend-kelasd',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => 'utf8_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
+        'pConnect'     => false
     ];
 
     /**
---
+++ app/Views/insert_product.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="<?=base_url("insertproducts/")?>">
+        <label for="nama_product">Nama Produk</label>
+        <input id="nama_product" name="nama_product"  type="text"></input>
+        <label for="description">Deskripsi Produk</label>
+        <input id="description" name="description" type="text"></input>
+        <button type="submit" >Tambah Produk</button>
+    </form>
+</body>
+</html>
+</body>
+</html>
